2a12be921bc8aba916904bb53c39bda79b65cf23
Joe <joquiros@estudiantec.cr>
2023-04-07 10:16:48 -0600

    Actualización de GUI
    Subo cambios realizados a la interfaz de usuario

--- build-untitled-Desktop_Qt_6_4_3_MinGW_64_bit-Debug/qtcsettings.cmake
+++ build-untitled-Desktop_Qt_6_4_3_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -1,2 +1,3 @@
 # This file is managed by Qt Creator, do not edit!
 
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/QUIROS CALVO/Trabajos_TEC_2023/ED_/I Proyecto/build-untitled-Desktop_Qt_6_4_3_MinGW_64_bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
